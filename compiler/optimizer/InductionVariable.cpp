8f3044cd1de952a18fcc6856be7611c2fc497cbb
Shubham Verma <shubhamv.sv@gmail.com>
2024-10-24 23:56:17 -0400

    fixup! Fix induction var reassociation in loop strider

--- compiler/optimizer/InductionVariable.cpp
+++ compiler/optimizer/InductionVariable.cpp
@@ -3709,8 +3709,10 @@ bool TR_LoopStrider::reassociateAndHoistComputations(TR::Block *loopInvariantBlo
          TR::Node *newLoad = TR::Node::createWithSymRef(node, TR::aload, 0, internalPointerSymRef);
          newLoad->setLocalIndex(~0);
          originalNode->setAndIncChild(0, newLoad);
-         originalNode->setAndIncChild(1, node->getFirstChild());
+         originalNode->setAndIncChild(1, node);
+         // originalNode->setAndIncChild(1, node->getFirstChild());
          reassociatedComputation = true;
+         traceMsg(comp(), "originalNode: %p, newLoad node: %p\n", originalNode, newLoad);
          }
       }
 #endif /* J9_PROJECT_SPECIFIC */
